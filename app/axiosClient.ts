b67b0e7f3b9b7e7820c55fdd6e2b4e0d67c296a0
Denis <kbs.pstu@gmail.com>
2023-11-10 17:34:40 +0500

    add url 6

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -9,7 +9,7 @@ import axios from 'axios'
 const instanse = axios.create({
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
   method: 'get',
-  timeout: 3000,
+  timeout: 5000,
   withCredentials: true,
   baseURL: 'https://shopfishsite.onrender.com',
 })
