c8478c19fc23fb9304b3d52efcf3e882049aab7a
Denis <kbs.pstu@gmail.com>
2023-11-10 14:13:51 +0500

    add url 3

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios'
 
-// const instance = axios.create({
-//   withCredentials: true,
-//   baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-// })
-
 const instanse = axios.create({
   withCredentials: true,
-  baseURL: 'https://shopfishsite.onrender.com',
+  baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
 })
+
+// const instanse = axios.create({
+//   withCredentials: true,
+//   baseURL: 'https://shopfishsite.onrender.com',
+// })
 
 export default instanse
